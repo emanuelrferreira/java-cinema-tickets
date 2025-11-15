a7eda7242b6c1b6ad0f9fd83dd7c65a632a64ca8
Emanuel Ferreira <decoding.dino@gmail.com>
2025-11-15 16:41:14 +0000

    feat: add CinemaAPP main application

---
+++ src/CinemaAPP.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+public class CinemaAPP {
+    public static void main(String[] args) {
+        //vars
+        int userAge;
+        int ticketsNum;
+        int totalPrice;
+
+        //objs
+        Scanner keyboard=new Scanner(System.in);
+        Cinema c=new Cinema();
+
+        //inputs
+        System.out.println("What is your age?");
+        userAge=keyboard.nextInt();
+        c.setUserAge(userAge);
+
+        System.out.println("How many tickets?");
+        ticketsNum=keyboard.nextInt();
+        c.setTicketsNum(ticketsNum);
+        
+        //process
+        c.computeTotalPrice();
+
+        //output
+        totalPrice=c.getTotalPrice();
+        System.out.println("Total to be pay is: "+totalPrice);
+        keyboard.close();
+    }
+}
